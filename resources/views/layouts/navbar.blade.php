60441a4449f29d92ded149bdee7b7bc1f603496b
firmant26 <andayanabwi@gmail.com>
2026-05-30 22:21:01 +0700

    perubahan pada sidebar
    saya merubah di folder layouts di file main, navbar, sidebar. supaya sidebarnya bisa dikecilkan dan dibesarkan

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,10 @@
-<nav class="bg-white p-4 flex justify-end items-center w-full">
+<nav class="bg-white p-4 flex justify-between items-center w-full border-b border-gray-100 shadow-sm">
+
+    <div class="flex items-center">
+        <button @click="sidebarOpen = !sidebarOpen" class="p-2 text-gray-500 hover:text-teal-600 hover:bg-gray-50 rounded-lg transition focus:outline-none">
+            <i class="fa-solid fa-bars text-lg"></i>
+        </button>
+    </div>
 
     <div class="flex items-center space-x-4">
         {{-- ALPINE.JS WRAPPER UNTUK DROPDOWN --}}
@@ -14,7 +20,7 @@
                 @endif
             </button>
 
-            {{-- KOTAK DROPDOWN MELAYANG (Ini yang sebelumnya terhapus) --}}
+            {{-- KOTAK DROPDOWN MELAYANG --}}
             <div x-show="open"
                 @click.away="open = false"
                 x-transition:enter="transition ease-out duration-200"
@@ -72,9 +78,43 @@
         </div>
 
         {{-- PROFIL USER --}}
-        <div class="flex items-center space-x-2">
-            <img src="{{ asset('images/user-profile-avatar.jpg') }}" class="w-8 h-8 rounded-xl object-cover border border-gray-100 shadow-sm" alt="Profile">
-        </div>
+        <div class="relative" x-data="{ openProfile: false }">
+            <button @click="openProfile = !openProfile" class="flex items-center space-x-1.5 focus:outline-none group">
+                <img src="{{ asset('images/user-profile-avatar.jpg') }}"
+                     class="w-8 h-8 rounded-xl object-cover border border-gray-100 shadow-sm group-hover:border-teal-500 transition"
+                     alt="Profile">
+            </button>
 
+            <div x-show="openProfile"
+                @click.away="openProfile = false"
+                x-transition:enter="transition ease-out duration-200"
+                x-transition:enter-start="opacity-0 scale-95"
+                x-transition:enter-end="opacity-100 scale-100"
+                class="absolute right-0 mt-4 w-56 bg-white rounded-xl shadow-xl border border-gray-100 z-[999] overflow-hidden py-1.5"
+                style="display: none;">
+
+                <a href="#" class="flex items-center px-4 py-2.5 text-sm text-gray-700 hover:bg-gray-50 transition">
+                    <i class="fa-regular fa-user w-5 text-gray-400 text-base mr-2.5"></i>
+                    <span>Lihat Profil</span>
+                </a>
+
+                <hr class="border-gray-100 my-1">
+
+                <div class="px-4 py-2 hover:bg-red-50/40 transition group">
+                    <form action="{{ route('logout') }}" method="POST" id="logoutForm">
+                        @csrf
+                        <button type="submit" class="w-full flex items-start text-left focus:outline-none">
+                            <i class="fa-solid fa-arrow-right-from-bracket w-5 text-red-500 text-base mr-2.5 mt-0.5"></i>
+                            <div>
+                                <span class="block text-sm font-semibold text-red-600">Log Out</span>
+                                <span class="block text-[11px] text-gray-400 mt-0.5">
+                                    {{ Auth::user()->name ?? 'Dr. Adhi Winardi' }}
+                                </span>
+                            </div>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </nav>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,66 +1,92 @@
-<aside class="w-64 bg-white h-screen border-r border-gray-100 hidden md:block" style="background-color: #FAFAFA">
-    <div class="p-6">
-        <h1 class="text-xl font-bold text-[#1b5e4b]">Klinik Winardi</h1>
-        <p class="text-[10px] text-gray-400 uppercase tracking-tight">Sistem Manajemen Protesa</p>
+<aside
+    class="bg-white h-screen border-r border-gray-100 hidden md:flex flex-col justify-between transition-all duration-300 flex-shrink-0"
+    :class="sidebarOpen ? 'w-64' : 'w-20'"
+    style="background-color: #FAFAFA">
+
+    <div>
+        <div class="p-6 overflow-hidden whitespace-nowrap">
+            <h1 class="text-xl font-bold text-[#1b5e4b] transition-all duration-300"
+                :class="sidebarOpen ? '' : 'text-center text-sm'">
+                <span x-show="sidebarOpen">Klinik Winardi</span>
+                <span x-show="!sidebarOpen"><i class="fa-solid fa-tooth"></i></span>
+            </h1>
+            <p x-show="sidebarOpen" class="text-[10px] text-gray-400 uppercase tracking-tight mt-1">Sistem Manajemen Protesa</p>
+        </div>
+
+        {{-- KUMPULAN VARIABEL STYLE --}}
+        @php
+            $baseClass     = "flex items-center p-3 rounded-md transition duration-200 ";
+            $activeClass   = "text-[#1b5e4b] bg-white font-medium shadow-md";
+            $inactiveClass = "text-gray-500 hover:bg-gray-50";
+        @endphp
+
+        <nav class="mt-4 px-4">
+            <ul class="space-y-2">
+                <li>
+                    <a href="{{ route('dashboard') }}"
+                       class="{{ $baseClass }} {{ request()->routeIs('dashboard') ? $activeClass : $inactiveClass }} :class="sidebarOpen ? '' : 'justify-center'"
+                       title="Dashboard">
+                        <i class="fa-solid fa-table-columns w-6 text-center text-lg :class="sidebarOpen ? 'mr-2' : ''"></i>
+                        <span x-show="sidebarOpen" class="whitespace-nowrap">Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('dokter.index') }}"
+                       class="{{ $baseClass }} {{ request()->is('data-master*') ? $activeClass : $inactiveClass }}"
+                       :class="sidebarOpen ? '' : 'justify-center'"
+                       title="Data Master">
+                        <i class="fa-solid fa-database w-6 text-center text-lg :class="sidebarOpen ? 'mr-2' : ''"></i>
+                        <span x-show="sidebarOpen" class="whitespace-nowrap">Data Master</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('pasien.index') }}"
+                       class="{{ $baseClass }} {{ request()->is('pasien*') ? $activeClass : $inactiveClass }}"
+                       :class="sidebarOpen ? '' : 'justify-center'"
+                       title="Pasien">
+                        <i class="fa-solid fa-users w-6 text-center text-lg :class="sidebarOpen ? 'mr-2' : ''"></i>
+                        <span x-show="sidebarOpen" class="whitespace-nowrap">Pasien</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('pemeriksaan.index') }}"
+                       class="{{ $baseClass }} {{ request()->is('pemeriksaan*') ? $activeClass : $inactiveClass }}"
+                       :class="sidebarOpen ? '' : 'justify-center'"
+                       title="Pemeriksaan">
+                        <i class="fa-solid fa-stethoscope w-6 text-center text-lg :class="sidebarOpen ? 'mr-2' : ''"></i>
+                        <span x-show="sidebarOpen" class="whitespace-nowrap">Pemeriksaan</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('pemesanan.index') }}"
+                       class="{{ $baseClass }} {{ request()->is('pemesanan*') ? $activeClass : $inactiveClass }}"
+                       :class="sidebarOpen ? '' : 'justify-center'"
+                       title="Pemesanan">
+                        <i class="fa-solid fa-cart-shopping w-6 text-center text-lg :class="sidebarOpen ? 'mr-2' : ''"></i>
+                        <span x-show="sidebarOpen" class="whitespace-nowrap">Pemesanan</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('pemesanan-riwayat') }}"
+                       class="{{ $baseClass }} {{ request()->is('riwayat-pemesanan*') ? $activeClass : $inactiveClass }}"
+                       :class="sidebarOpen ? '' : 'justify-center'"
+                       title="Riwayat Pemesanan">
+                        <i class="fa-solid fa-clock-rotate-left w-6 text-center text-lg :class="sidebarOpen ? 'mr-2' : ''"></i>
+                        <span x-show="sidebarOpen" class="whitespace-nowrap">Riwayat Pemesanan</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 
-    {{-- KUMPULAN VARIABEL STYLE AGAR RAPI & REUSABLE --}}
-    @php
-        $baseClass     = "flex items-center p-3 rounded-md transition ";
-        $activeClass   = "text-[#1b5e4b] bg-white font-medium shadow-md";
-        $inactiveClass = "text-gray-500 hover:bg-gray-50";
-    @endphp
-
-    <nav class="mt-4 px-4">
-        <ul class="space-y-2">
-
-            <li>
-                <a href="{{ route('dashboard') }}"
-                   class="{{ $baseClass }} {{ request()->routeIs('dashboard') ? $activeClass : $inactiveClass }}">
-                    <i class="fa-solid fa-table-columns w-6 text-center mr-2"></i>
-                    <span>Dashboard</span>
-                </a>
-            </li>
-
-            <li>
-                <a href="{{ route('dokter.index') }}"
-                   class="{{ $baseClass }} {{ request()->is('data-master*') ? $activeClass : $inactiveClass }}">
-                    <i class="fa-solid fa-database w-6 text-center mr-2"></i>
-                    <span>Data Master</span>
-                </a>
-            </li>
-
-            <li>
-                <a href="{{ route('pasien.index') }}"
-                   class="{{ $baseClass }} {{ request()->is('pasien*') ? $activeClass : $inactiveClass }}">
-                    <i class="fa-solid fa-users w-6 text-center mr-2"></i>
-                    <span>Pasien</span>
-                </a>
-            </li>
-
-            <li>
-                <a href="{{ route('pemeriksaan.index') }}"
-                   class="{{ $baseClass }} {{ request()->is('pemeriksaan*') ? $activeClass : $inactiveClass }}">
-                    <i class="fa-solid fa-stethoscope w-6 text-center mr-2"></i>
-                    <span>Pemeriksaan</span>
-                </a>
-            </li>
-
-            <li>
-                <a href="{{ route('pemesanan.index') }}"
-                   class="{{ $baseClass }} {{ request()->is('pemesanan*') ? $activeClass : $inactiveClass }}">
-                    <i class="fa-solid fa-cart-shopping w-6 text-center mr-2"></i>
-                    <span>Pemesanan</span>
-                </a>
-            </li>
-
-             <li>
-                <a href="{{ route('pemesanan-riwayat') }}"
-                   class="{{ $baseClass }} {{ request()->is('riwayat-pemesanan*') ? $activeClass : $inactiveClass }}">
-                    <i class="fa-solid fa-clock-rotate-left w-6 text-center mr-2"></i>
-                    <span>Riwayat Pemesanan</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
+    <div class="p-4 border-t border-gray-100 flex" :class="sidebarOpen ? 'justify-end' : 'justify-center'">
+        <button @click="sidebarOpen = !sidebarOpen" class="p-2 text-gray-400 hover:text-[#1b5e4b] hover:bg-gray-100 rounded-md transition focus:outline-none">
+            <i class="fa-solid text-base" :class="sidebarOpen ? 'fa-angle-left' : 'fa-angle-right'"></i>
+        </button>
+    </div>
 </aside>
